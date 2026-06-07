7ffd72dedb3a8eafba89a42c3b8e3ee2d6b92d0b
Nicolò Bertocco <nick.bertocco@gmail.com>
2026-06-07 18:32:04 +0200

    Fix erroneous user info middleware configuration to exclude public endpoints

--- services/workout-service/src/WorkoutService.Api/Common/Auth/UserInfoMiddleware.cs
+++ services/workout-service/src/WorkoutService.Api/Common/Auth/UserInfoMiddleware.cs
@@ -12,31 +12,36 @@ public sealed class UserInfoMiddleware(ILogger<UserInfoMiddleware> logger) : IMi
 {
   public async Task InvokeAsync(HttpContext context, RequestDelegate next)
   {
-    if (context.Request.Headers.TryGetValue("X-Userinfo", out var headerValue))
+    if (context.Request.Path.StartsWithSegments("/api"))
     {
-      try
+      if (context.Request.Headers.TryGetValue("X-Userinfo", out var headerValue))
       {
-        var json = Encoding.UTF8.GetString(Convert.FromBase64String(headerValue.ToString()));
-        var root = JsonSerializer.Deserialize<JsonElement>(json);
+        try
+        {
+          var json = Encoding.UTF8.GetString(Convert.FromBase64String(headerValue.ToString()));
+          var root = JsonSerializer.Deserialize<JsonElement>(json);
 
-        var claims = new List<Claim>();
+          var claims = new List<Claim>();
 
-        if (root.TryGetProperty("preferred_username", out var username))
-          claims.Add(new Claim(ClaimTypes.Name, username.GetString()!));
-        else
-          throw new ForbiddenException("Missing preferred_username field from X-Userinfo request header");
+          if (root.TryGetProperty("preferred_username", out var username))
+            claims.Add(new Claim(ClaimTypes.Name, username.GetString()!));
+          else
+            throw new ForbiddenException("Missing preferred_username field from X-Userinfo request header");
 
-        context.User = new ClaimsPrincipal(new ClaimsIdentity(claims, "Gateway"));
+          context.User = new ClaimsPrincipal(new ClaimsIdentity(claims, "Gateway"));
+        }
+        catch (Exception ex)
+        {
+          logger.LogCritical(ex, "Failed to parse X-Userinfo header.");
+          throw;
+        }
       }
-      catch (Exception ex)
+      else
       {
-        logger.LogCritical(ex, "Failed to parse X-Userinfo header.");
-        throw;
+        throw new ForbiddenException("Missing X-Userinfo request header");
       }
-    }
-    else
-    {
-      throw new ForbiddenException("Missing X-Userinfo request header");
+
+      await next(context);
     }
 
     await next(context);
